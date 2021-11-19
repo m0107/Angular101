c6a1fa805561c0ab85e40ed9c98c0df3ff669ae3
akshata <akshata.m@speedyy.com>
2021-11-19 10:30:28 +0530

    merge

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,14 +3,44 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
+import { LoginComponent } from './components/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { UserListComponent } from './components/userList/userList.component';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+
+const appRoutes:Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'userList',
+    component: UserListComponent
+  },
+  {
+    path: '',
+    component: DashboardComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    LoginComponent,
+    UserListComponent,
+    DashboardComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ front/src/app/components/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  email!: string;
+  password!: string;
+  constructor(private router:Router) {}
+
+  ngOnInit(): void {}
+
+  onLogin() {
+    if (!this.email) {
+      alert('Login data missing!');
+    } else { alert('User Logged!');
+    this.router.navigate(['userList']);
+    }
+  }
+  
+}
