31eb3df6bdfa62cf6c2e4b1b3edd1265c22e595b
akshata <akshata.m@speedyy.com>
2021-11-26 18:28:58 +0530

    databinding/pipes

--- apicall/src/app/app.component.ts
+++ apicall/src/app/app.component.ts
@@ -7,8 +7,29 @@ import {GetApiService} from './get-api.service';
 })
 export class AppComponent {
   title = 'apicall';
-  user_id = '1';
-  id = '1';
+  user_id = 'apicall1';
+  id = 'apicall';
+  title1 = 'apicall1';
+  user_id1 = 'apicall1';
+  id1 = 'apicall1';
+  title2 = 'apicall2';
+  user_id2 = 'apicall2';
+  id2 = 'apicall2';
+  title3 = 'apicall3';
+  user_id3 = 'apicall3';
+  id3 = 'apicall3';
+  title4 = 'apicall4';
+  user_id4 = '1apicall4';
+  id4 = 'apicall4';
+  title5 = 'apicall5';
+  user_id5 = 'apicall5';
+  id5 = 'apicall5';
+  title6 = 'apicall6';
+  user_id6 = '1apicall6';
+  id6 = 'apicall6';
+  title7 = 'apicall7';
+  user_id7 = '1apicall7';
+  id7 = 'apicall7';
   constructor(private api:GetApiService)
   {
 
@@ -18,6 +39,57 @@ export class AppComponent {
     this.api.apiCall().subscribe((data:any)=>{
       console.warn("get api data",data);
       this.title=data['title'];
+      this.id=data['id'];
+      this.user_id=data['userId'];
+    })
+
+    this.api.apiCall1().subscribe((data:any)=>{
+      console.warn("get api data",data);
+      this.title1=data['title'];
+      this.id1=data['id'];
+      this.user_id1=data['userId'];
+    })
+
+    this.api.apiCall2().subscribe((data:any)=>{
+      console.warn("get api data",data);
+      this.title2=data['title'];
+      this.id2=data['id'];
+      this.user_id2=data['userId'];
+    })
+
+    this.api.apiCall3().subscribe((data:any)=>{
+      console.warn("get api data",data);
+      this.title3=data['title'];
+      this.id3=data['id'];
+      this.user_id3=data['userId'];
+    })
+
+    this.api.apiCall4().subscribe((data:any)=>{
+      console.warn("get api data",data);
+      this.title4=data['title'];
+      this.id4=data['id'];
+      this.user_id4=data['userId'];
+    })
+
+    this.api.apiCall5().subscribe((data:any)=>{
+      console.warn("get api data",data);
+      this.title5=data['title'];
+      this.id5=data['id'];
+      this.user_id5=data['userId'];
+    })
+
+    this.api.apiCall6().subscribe((data:any)=>{
+      console.warn("get api data",data);
+      this.title6=data['title'];
+      this.id6=data['id'];
+      this.user_id6=data['userId'];
+    })
+
+    this.api.apiCall7().subscribe((data:any)=>{
+      console.warn("get api data",data);
+      this.title7=data['title'];
+      this.id7=data['id'];
+      this.user_id7=data['userId'];
     })
   }
 }
--- apicall/src/app/get-api.service.ts
+++ apicall/src/app/get-api.service.ts
@@ -13,4 +13,39 @@ export class GetApiService {
   {
     return this.http.get('https://jsonplaceholder.typicode.com/todos/1');
   }
+
+  apiCall1()
+  {
+    return this.http.get('https://jsonplaceholder.typicode.com/todos/2');
+  }
+
+  apiCall2()
+  {
+    return this.http.get('https://jsonplaceholder.typicode.com/todos/3');
+  }
+
+  apiCall3()
+  {
+    return this.http.get('https://jsonplaceholder.typicode.com/todos/4');
+  }
+
+  apiCall4()
+  {
+    return this.http.get('https://jsonplaceholder.typicode.com/todos/5');
+  }
+
+  apiCall5()
+  {
+    return this.http.get('https://jsonplaceholder.typicode.com/todos/6');
+  }
+
+  apiCall6()
+  {
+    return this.http.get('https://jsonplaceholder.typicode.com/todos/7');
+  }
+
+  apiCall7()
+  {
+    return this.http.get('https://jsonplaceholder.typicode.com/todos/3');
+  }
 }
